9940c5556a60674cc2c03aa7bfe7a5bcebbd17dc
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-06-23 14:51:26 +0200

    second Commid

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
 import 'package:oop/oop4.dart' as oop;
 
 void main(List<String> arguments) {
-  print('Hello world: ${oop.calculate()}!');
+  Teilnehmer(firstName, secondName, age, email, adress, gender)  
 }
--- lib/oop4.dart
+++ lib/oop4.dart
@@ -1,3 +1,38 @@
-int calculate() {
-  return 6 * 7;
+enum Gender { mann, frau }
+
+class Teilnehmer {
+  String? firstName;
+  String? secondName;
+  int? age;
+  String? email;
+  Adress? adress;
+  Gender? gender;
+  bool? zutriffsberechtigung = true;
+
+  Teilnehmer(
+    this.firstName,
+    this.secondName,
+    this.age,
+    this.email,
+    this.adress,
+    this.gender, {
+    this.zutriffsberechtigung,
+  });
+}
+
+class Adress {
+  String? cytyName;
+  String? streetName;
+  int? plz;
+  int? houseNummber;
+
+  Adress(this.cytyName, this.streetName, this.plz, this.houseNummber);
+}
+
+class Kurse {
+  String dart = 'Dart';
+  String java = 'Java';
+  String html = 'Html';
+  String oop = 'OOP';
+  Teilnehmer? teilnehmer;
 }
