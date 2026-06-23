9c14d25c18ddfea3078383f7007ac68026ae6570
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-06-23 11:33:45 +0200

    second Commid

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:oop/oop4.dart' as oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oop.calculate()}!');
+}
---
+++ test/oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:oop/oop4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
